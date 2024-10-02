4fa8be58e913758579af6cd320cfd04a5783342b
Luke Frandsen <168469565+LukeFrandsen@users.noreply.github.com>
2024-10-02 17:40:13 -0600

    testing

--- testing.cpp
+++ testing.cpp
@@ -5,7 +5,8 @@ int main(){
     int age = 15;
     std::string name = "John";
     std::cout << "Name: " << name << " Age: " << age << std::endl;
-    if (age > 18){
+
+    if (age > 18){ // this is an if statement that will check if age is greater than 18
         std::cout << "You are an adult" << std::endl;
     } else if (age == 15){
         if (name == "John"){
@@ -19,7 +20,7 @@ int main(){
     std::cout << "Your grade is: ";
     std::cin >> grade;
 
-    switch (grade){
+    switch (grade){ // this is a switch statement that will check the value of grade
         case 1 :
             std::cout << "You got a A" << std::endl;
             break;
@@ -35,6 +36,16 @@ int main(){
         default:
             std::cout << "unknwn grade" << std::endl;
     }
+
+    for(int i = 0; i < 5; i++){ // this is a for loop that will run 5 times
+        std::cout << "Iteration: " << i << std::endl;
+    }
+    int i = 5;
+    while(i< 18){ // this is a while loop that will run as long as age is less than 18
+        std::cout << "You are a minor" << std::endl;
+        i++;
+    }
+
 
     return 0;
 
